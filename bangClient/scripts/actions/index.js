3535d2721a745d551fb2142897f00e472aa11d70
Ted Shaffer <ted@brightsign.biz>
2016-06-07 19:58:36 -0700

    additional thunk cleanup

--- bangClient/scripts/actions/index.js
+++ bangClient/scripts/actions/index.js
@@ -21,45 +21,24 @@ export function receiveThumbs(thumbs) {
 }
 
 export function fetchMediaFolder() {
-    console.log("fetchMediaFolder invoked");
+
     return function (dispatch) {
 
-        console.log("fetchMediaFolder perform fetch");
-
-        // return fetch(`http://localhost:6969/getMediaFolder`)
-        //     .then(response => response.json())
-        //     .then(function(data) {
-        //         const mediaFolder = data.mediaFolder;
-        //         dispatch(receiveMediaFolder(mediaFolder));
-        //     });
         fetch(`http://localhost:6969/getMediaFolder`)
             .then(response => response.json())
             .then(function(data) {
+
                 const mediaFolder = data.mediaFolder;
+
                 dispatch(receiveMediaFolder(mediaFolder));
 
-                const url = "http://localhost:6969/";
-                const getThumbsUrl = url + "getThumbs";
+                const getThumbsUrl = "http://localhost:6969/" + "getThumbs";
 
                 return axios.get(getThumbsUrl, {
                     params: { mediaFolder: mediaFolder }
                 }).then(function(data) {
-                    console.log("successful return from axios.get");
-                    console.log("Set a breakpoint here");
-                    var thumbs = data.data.thumbs;
-                    console.log("number of thumbs=" + thumbs.length);
-                    dispatch(receiveThumbs(thumbs));
+                    dispatch(receiveThumbs(data.data.thumbs));
                 });
-
-                // const request = axios.get(getThumbsUrl, {
-                //     params: { mediaFolder: mediaFolder }
-                // });
-
-                // return fetch(getThumbsUrl)
-                //     .then(response => response.json())
-                //     .then(function(data) {
-                //         console.log("received response from getThumbs");
-                //     });
             });
     }
 };
@@ -69,27 +48,6 @@ import axios from 'axios';
 
 export const GET_MEDIA_FOLDER = 'GET_MEDIA_FOLDER';
 export const GET_THUMBS = 'GET_THUMBS';
-
-export function getMediaFolder() {
-
-    console.log("getMediaFolder invoked");
-    //
-    // const url = "http://localhost:6969/";
-    //
-    // const getMediaFolderUrl = url + "getMediaFolder";
-    //
-    // $.get( getMediaFolderUrl, function( mediaFolder ) {
-    //     console.log("getMediaFolder jquery call returned: " + mediaFolder);
-    // });
-    //
-    // const request = axios.get(getMediaFolderUrl, {});
-    //
-    // return {
-    //     type: GET_MEDIA_FOLDER,
-    //     payload: request
-    // };
-}
-
 
 export function getThumbs(mediaFolder) {
     
@@ -101,20 +59,6 @@ export function getThumbs(mediaFolder) {
     const updateMediaFolderRequest = axios.get(updateMediaFolderUrl, {
         params: { mediaFolder: mediaFolder }
     });
-
-    // test code
-    // updateMediaFolderRequest.then(function(data) {
-    //     const getThumbsUrl = url + "getThumbs";
-    //     const request = axios.get(getThumbsUrl, {
-    //         params: { mediaFolder: mediaFolder }
-    //     });
-    //
-    //     return {
-    //         type: GET_THUMBS,
-    //         payload: request
-    //     };
-    //
-    // });
 
     const getThumbsUrl = url + "getThumbs";
     const request = axios.get(getThumbsUrl, {
--- bangClient/scripts/reducers/index.js
+++ bangClient/scripts/reducers/index.js
@@ -5,20 +5,9 @@ import {combineReducers} from 'redux';
 import ThumbsReducer from './reducer_thumbs';
 import MediaFolderReducer from './reducer_media_folder';
 
-// import { REQUEST_MEDIA_FOLDER } from '../actions/index';
-
 const rootReducer = combineReducers({
     thumbs: ThumbsReducer,
     mediaFolder: MediaFolderReducer,
 });
-
-// function receivedMediaFolder(state = [], action) {
-//     switch (action.type) {
-//         case REQUEST_MEDIA_FOLDER:
-//             return "";
-//         default:
-//             return state;
-//     }
-// }
 
 export default rootReducer;
