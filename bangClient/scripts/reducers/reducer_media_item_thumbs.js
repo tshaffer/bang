0fcc1d5d6073c28bd2de5453cb4dfc9e15e134a3
Ted Shaffer <ted@brightsign.biz>
2016-06-09 12:31:41 -0700

    properly merge data in thumbs reducer.

--- bangClient/scripts/reducers/reducer_media_item_thumbs.js
+++ bangClient/scripts/reducers/reducer_media_item_thumbs.js
@@ -8,7 +8,19 @@ export default function(state = [], action) {
 
     switch (action.type) {
         case SET_MEDIA_LIBRARY_FILES:
-            return action.payload.mediaItemThumbs;
+            console.log("pizza 1");
+            console.log("pizza 2");
+
+            let newMediaItemThumbs = action.payload.mediaItemThumbs;
+
+            // merge existing thumbs with new thumbs - merging in this direction avoid mutating current state
+            for (var prop in state) {
+                if (state.hasOwnProperty( prop ) &&  !newMediaItemThumbs.hasOwnProperty( prop )) {
+                    newMediaItemThumbs[prop] = state[prop];
+                }
+            }
+
+            return newMediaItemThumbs;
     }
 
     return state;
