a37bfe21b2a44c9be3940c3be8394001019793cf
Ted Shaffer <ted@brightsign.biz>
2016-06-15 20:07:05 -0700

    experimental code to exercise indexedDB

--- bangClient/scripts/containers/ba.js
+++ bangClient/scripts/containers/ba.js
@@ -28,9 +28,74 @@ class BA extends Component {
 
         this.baUI = new BAUI(this);
 
+        this.db = null;
+        this.dbName = "BADatabase-0";
     }
 
     componentWillMount() {
+
+        // indexedDB tests
+        var request = window.indexedDB.open(this.dbName, 4);
+
+        request.onerror = function(event) {
+            // Do something with request.errorCode!
+            alert("Database error: " + event.target.errorCode);
+        };
+        request.onupgradeneeded = function(event) {
+            console.log("openDB.onupgradeneeded invoked");
+            this.db = event.target.result;
+
+            var store = this.db.createObjectStore("thumbFiles");
+
+            store.transaction.oncomplete = function(event) {
+                console.log("createObjectStore transaction complete");
+                console.log("poop1");
+                console.log("poop2");
+                // // Store values in the newly created objectStore.
+                // var transaction = this.db.transaction(["thumbFiles"], "readwrite");
+                //
+                // var request = objectStore.add();
+                // request.onsuccess = function(event) {
+                //     // event.target.result == customerData[i].ssn;
+                // };
+                // var thumbFilesObjectStore = this.db.transaction("thumbFiles", "readwrite").objectStore("thumbFiles");
+                // thumbFilesObjectStore.add("thumb1.jpg", "/users/tedshaffer/Projects/thumb1.jpg");
+                // thumbFilesObjectStore.add("thumb2.jpg", "/users/tedshaffer/Projects/thumb2.jpg");
+            };
+        };
+        request.onsuccess = function(event) {
+            // Do something with request.result!
+            console.log("request.onsuccess invoked");
+            console.log("poop1");
+            console.log("poop2");
+            this.db = event.target.result;
+
+            // var tx = this.db.transaction("thumbFiles", "readwrite");
+            // let objectStore = tx.objectStore("thumbFiles");
+            // var request = objectStore.add("/users/tedshaffer/Projects/thumb3.jpg", "thumb3");
+            // request.onsuccess = function(event) {
+            //     console.log("add success 1");
+            //     console.log("add success 2");
+            //     var returnedKey = event.target.result;
+            //     console.log("returnedKey=", returnedKey);
+            // };
+
+            var objectStore = this.db.transaction("thumbFiles").objectStore("thumbFiles");
+
+            objectStore.openCursor().onsuccess = function(event) {
+                var cursor = event.target.result;
+                if (cursor) {
+                    console.log("Item " + cursor.key + " is " + cursor.value);
+                    cursor.continue();
+                }
+                else {
+                    console.log("No more entries!");
+                }
+            };
+        };
+
+
+
         this.props.getAllThumbs();
     }
 
