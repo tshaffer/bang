7b590edd7b9cc2b056c2e9e30436954e565891c1
Ted Shaffer <ted@brightsign.biz>
2016-10-14 08:54:39 -0700

    more deletions

--- bangClient/scripts/containers/ba.js
+++ bangClient/scripts/containers/ba.js
@@ -45,84 +45,13 @@ class BA extends Component {
         this.baUI.init();
     }
 
-    getEmptyPlaylistItem(existingPlaylistItem) {
-
-        let emptyPlaylistItem = null;
-        const emptyImagePlaylistItem = new ImagePlaylistItem();
-        const emptyHTML5PlaylistItem = new HTML5PlaylistItem();
-        if (existingPlaylistItem instanceof ImagePlaylistItem) {
-            emptyPlaylistItem = emptyImagePlaylistItem;
-        }
-        else if (existingPlaylistItem instanceof HTML5PlaylistItem) {
-            emptyPlaylistItem = emptyHTML5PlaylistItem;
-        }
-        else {
-            console.log("existingPlaylistItem: unknown playlistItem type");
-        }
-        return emptyPlaylistItem;
-    }
-
-
-    copyExistingPlaylistItem(selectedPlaylistItemId) {
-
-        const existingPlaylistItem = this.props.playlistItems.playlistItemsById[selectedPlaylistItemId];
-        let emptyPlaylistItem = this.getEmptyPlaylistItem(existingPlaylistItem);
-        if (!emptyPlaylistItem) return null;
-
-        return Object.assign(emptyPlaylistItem, existingPlaylistItem);
-
-    }
-
-    getCurrentZone() {
-
-        let selectedZone = null;
-        if (this.props.sign && this.props.sign.zoneIds.length > 0 && this.props.zones && this.props.zones.zonesById) {
-
-            if (!this.state.selectedZone) {
-                selectedZone = this.props.zones.zonesById[this.props.sign.zoneIds[0]];
-            }
-            else {
-                selectedZone = this.state.selectedZone;
-            }
-
-            if (!selectedZone) {
-                selectedZone = null;
-            }
-        }
-        return selectedZone;
-    }
-
-    getCurrentZonePlaylist() {
-
-        let currentZonePlaylist = null;
-
-        let selectedZone = this.getCurrentZone();
-        if (selectedZone) {
-            currentZonePlaylist = this.props.zonePlaylists.zonePlaylistsById[selectedZone.zonePlaylistId];
-        }
-        return currentZonePlaylist;
-    }
-
     render () {
-
-        let signName = <span>No sign yet</span>;
-        let signVideoMode = <span>No videoMode yet</span>;
-        if (this.props.sign) {
-            signName = this.props.sign.name;
-            signVideoMode = this.props.sign.videoMode;
-        }
 
         const openSavePresentationJSX = <div>pizza!!!</div>;
 
         return (
 
             <div>
-
-                <div>
-                    <span>{signName}</span>
-                    :
-                    <span>{signVideoMode}</span>
-                </div>
 
                 <div className="bangPageContainer">
                     <MediaLibrary
@@ -132,13 +61,8 @@ class BA extends Component {
                         mediaThumbs={this.props.mediaThumbs}
                     />
                     <NonInteractivePlaylist
-                        sign={this.props.sign}
-                        zones={this.props.zones}
-                        zonePlaylists={this.props.zonePlaylists}
                         mediaThumbs={this.props.mediaThumbs}
                         selectedMediaStateId={this.state.selectedMediaStateId}
-                        bangSign={this.props.sign}
-                        bangZones={this.props.zones}
                     />
                 </div>
             </div>
@@ -151,20 +75,9 @@ function mapStateToProps(baState) {
     const state = baState.reducers;
 
     return {
-        sign: state.sign,
-        zones: state.zones,
-        zonePlaylists: state.zonePlaylists,
-        mediaStates: state.mediaStates,
-        playlistItems: state.playlistItems,
-        htmlSites: state.htmlSites,
-
         mediaLibraryPlaylistItems: state.mediaLibraryPlaylistItems,
         mediaFolder: state.mediaFolder,
         mediaThumbs: state.mediaThumbs,
-
-        bsnAuthData: state.bsnAuthData,
-
-        firmwareSpecs: state.firmwareSpecs
     };
 }
 
@@ -178,15 +91,9 @@ function mapDispatchToProps(dispatch) {
 BA.propTypes = {
     createDefaultPresentation: React.PropTypes.func.isRequired,
     loadAppData: React.PropTypes.func.isRequired,
-    sign: React.PropTypes.object.isRequired,
-    zones: React.PropTypes.object.isRequired,
-    zonePlaylists: React.PropTypes.object.isRequired,
-    mediaStates: React.PropTypes.object.isRequired,
-    playlistItems: React.PropTypes.object.isRequired,
     mediaFolder: React.PropTypes.string.isRequired,
     mediaThumbs: React.PropTypes.object.isRequired,
     mediaLibraryPlaylistItems: React.PropTypes.array.isRequired,
-    htmlSites: React.PropTypes.object.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(BA);
--- bangClient/scripts/reducers/index.js
+++ bangClient/scripts/reducers/index.js
@@ -6,29 +6,10 @@ import MediaThumbsReducer from './reducer_media_thumbs';
 import MediaFolderReducer from './reducer_media_folder';
 import MediaLibraryPlaylistItemsReducer from './reducer_media_library_playlist_items';
 
-import SignReducer from './reducer_sign';
-import ZonesReducer from './reducer_zones';
-import ZonePlaylistsReducer from './reducer_zone_playlists';
-import MediaStatesReducer from './reducer_media_states';
-import PlaylistItemsReducer from './reducer_playlist_items';
-import HtmlSitesReducer from './reducer_html_sites';
-
-import TransitionsReducer from './reducer_transitions';
-
-
 const rootReducer = combineReducers({
     mediaThumbs: MediaThumbsReducer,
     mediaFolder: MediaFolderReducer,
     mediaLibraryPlaylistItems: MediaLibraryPlaylistItemsReducer,
-    
-    sign: SignReducer,
-    zones: ZonesReducer,
-    zonePlaylists: ZonePlaylistsReducer,
-    mediaStates: MediaStatesReducer,
-    playlistItems: PlaylistItemsReducer,
-    htmlSites: HtmlSitesReducer,
-
-    transitions: TransitionsReducer,
 });
 
 export default rootReducer;
